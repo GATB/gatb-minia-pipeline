5953282e276ba88ad0a326d24a5551f98f8ba04f
Charles Deltel <Charles.Deltel@inria.fr>
2014-08-11 14:54:17 +0200

    adaptation to use BESST submodule
    Put submodule commands in the pipeline's common script.
    
    [Rayan's mail on 2014/07/14]

--- gatb-pipeline-common.sh
+++ gatb-pipeline-common.sh
@@ -95,6 +95,12 @@ EOF
 EXT_download_source_code() {
 	git clone git+ssh://cdeltel@scm.gforge.inria.fr//gitroot/gatb-pipeline/gatb-pipeline.git git-gatb-pipeline
 	[ $? -ne 0 ] && { echo "git clone error"; exit 1;}
+	cd git-gatb-pipeline
+	git submodule init
+	git submodule update
+	[ $? -ne 0 ] && { echo "git submodule error"; exit 1;}
+	git submodule status
+	cd ..
 	svn co svn+ssh://scm.gforge.inria.fr/svnroot/projetssymbiose/superscaffolder             superscaffolder
 	[ $? -ne 0 ] && { echo "svn co error"; exit 1;}
 	svn co svn+ssh://scm.gforge.inria.fr/svnroot/projetssymbiose/minia/trunk                 debloom
